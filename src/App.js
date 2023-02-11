01f09379c332663e858fbb1732fd7a3fe2ecf8ae
Vinh Nguyen <nguyenquocvinh813@gmail.com>
2023-02-12 00:54:47 +0700

    new banner

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import Details from './components/Details';
 import Detailbepgas from './components/detailbepgas';
 import listbepgas from './components/listbepgas'; 
 import Default from './components/Default';
-import SimpleMenu from "../src/components/menu/menu"
 
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
-      <SimpleMenu />
       <Switch>
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../logo.svg";
+import logo from "../logo.jpg";
 import styled from "styled-components";
 import { ButtonContainer } from "./Button";
 import GoogleLogin from "react-google-login";
@@ -9,9 +9,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Button, Modal } from "react-bootstrap";
+// import SimpleMenu from './menu/menu.js'
 
 const Navbar = () => {
   const [profile, setProfile] = useState("");
+  const [showLogin, setShowLogin] = useState(false);
+  const handleClose = () => setShowLogin(false);
   const clientId =
     "746537720400-on1e0j856ksf0h76k3n216bo4olb15f7.apps.googleusercontent.com";
   const clientSecret = "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";
@@ -44,82 +48,107 @@ const Navbar = () => {
       window.history.pushState({ urlPath: "/" }, "", "/");
       setProfile(JSON.parse(localStorage.getItem("profile")));
       notify();
+      setShowLogin(false);
       return true;
     } else {
       notiError();
+      setShowLogin(false);
       // document.body.classList.remove('body_login_page');
       return false;
     }
   };
 
   return (
-    <NavWrapper className="navbar navbar-expand-sm px-sm-5">
-      <div>
-        <ToastContainer />
-      </div>
-      <Link to="/">
-        <img src={logo} alt="store" className="navbar-brand" />
-        Trang chủ
-      </Link>
-      <ul className="navbar-nav align-items-center">
-        <li className="nav-item ml-5">
+    <>
+      <DivBanner className="container">
+        <button className="shopping mr-xl-4" title="Giỏ hàng của bạn">
+          <FontAwesomeIcon icon={faCartShopping} />
+        </button>
+        {profile?.email ? (
+          <div className="grid">
+            <span> Welcome: {profile?.email}</span>
+            <ButtonContainer
+              onClick={() => {
+                localStorage.removeItem("profile");
+                window.location.reload();
+              }}
+            >
+              logOut
+            </ButtonContainer>
+          </div>
+        ) : (
+          <ButtonContainer onClick={() => setShowLogin(!showLogin)}>
+            Đăng nhập
+          </ButtonContainer>
+        )}
+      </DivBanner>
+      <NavWrapper className="navbar navbar-expand-sm p-0 ">
+        <div>
+          <ToastContainer />
+        </div>
+        <div className="d-flex justify-content-between w-100">
+          <Link to="/">
+            <img src={logo} alt="store" width={300} height={300} />
+            {/* Trang chủ */}
+          </Link>
+          <ul className="navbar-nav align-items-center">
+        <li className="nav-item ">
           <Link to="/" className="nav-link">
             0942520449
+            <br/>
+            <span className="text-center"></span>
             <br />
             0949229321
           </Link>
         </li>
       </ul>
-      <ul className="navbar-nav align-items-center">
-        <li className="nav-item ml-5">
-          <Link to="/" className="nav-link">
-            Gas Khôi Nguyên Gas an toàn
-          </Link>
-        </li>
-      </ul>
-      <Link className="ml-auto">
-        <ButtonContainer>
-          <span className="mr-2">
-            <i className="fas fa-motorcycle"></i>
-          </span>
-          Giao hàng miễn phí tận nơi. <br />
-          Sẵn sàn phục vụ mọi nơi tại huyện Thạnh Phú .
-        </ButtonContainer>
-      </Link>
-      <button className="shopping" title="Giỏ hàng của bạn">
-        <FontAwesomeIcon icon={faCartShopping} />
-      </button>
-      {profile?.email ? (
-        <div className="">
-          <span> Welcome {profile?.email}</span>
-          <ButtonContainer
-            onClick={() => {
-              localStorage.removeItem("profile");
-              window.location.reload();
-            }}
-          >
-            logOut
-          </ButtonContainer>
+
+          {/* <div className="slowgen">
+            <span className="mr-2">
+              <i className="fas fa-motorcycle"></i>
+            </span>
+            Giao hàng miễn phí tận nơi. <br />
+            Sẵn sàn phục vụ mọi nơi tại huyện Thạnh Phú .
+          </div> */}
+          {/* <SimpleMenu /> */}
         </div>
-      ) : (
-        <GoogleLogin
-          // className={className}
-          clientId={clientId}
-          clientSecret={clientSecret}
-          buttonText="Continue with Google"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={"single_host_origin"}
-          // isSignedIn={true}
-        />
-      )}
-    </NavWrapper>
+        <Modal
+          show={showLogin}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+        >
+          <Modal.Header>
+            <Modal.Title>Đăng nhập</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <GoogleLogin
+              // className={className}
+              clientId={clientId}
+              clientSecret={clientSecret}
+              buttonText="Continue with Google"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              // isSignedIn={true}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Đóng
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </NavWrapper>
+    </>
   );
 };
 
 export default Navbar;
 const NavWrapper = styled.nav`
-  background-color: #a8d762;
+  background-image:url('https://gassaigonvina.com/upload/photo/slishow-1589350391-bannermain01-6840.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
   .nav-link {
     color: var(--mainWhite) !important;
     font-size: 1.3rem;
@@ -128,5 +157,24 @@ const NavWrapper = styled.nav`
   .shopping {
     background-color: transparent;
     border: none;
+  }
+  .slowgen {
+    align-items: center !important;
+    display: flex;
+    text-align: center;
+    color: blue;
+    font-size : 16px
+  }
+`;
+const DivBanner = styled.nav`
+  display: flex;
+  justify-content: end;
+  padding: 16px;
+  .shopping {
+    background-color: transparent;
+    border: none;
+  }
+  .grid {
+    display: grid;
   }
 `;
